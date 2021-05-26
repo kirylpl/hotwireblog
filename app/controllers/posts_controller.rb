17a89f8a655265cea490d1bcb3fe55a5042c6592
Kiryl Plyashkevich <kiryl.ply@gmail.com>
2021-05-26 09:59:39 +0300

    add real-time updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   def index
     @posts = Post.all
+    @post = Post.new
   end
 
   # GET /posts/1
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  # after_create_commit {broadcast_to 'posts'}
+  # after_update_commit {}
+  broadcasts_to ->(post) {:posts}
+
   belongs_to :user
   has_many :comments
   validates :body, presence: true
